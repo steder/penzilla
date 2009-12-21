33b26a9a8f08a0d074da5e2a49cfda1981163f56
Mike Steder <steder@gmail.com>
2009-12-21 11:48:24 -0600

    Got the basic thing working.

--- server/main.py
+++ server/main.py
@@ -1,5 +1,7 @@
 """
 """
+import os
+
 from twisted.internet import reactor
 from twisted.python import log
 from twisted.python import filepath
@@ -8,13 +10,14 @@ from twisted.web.resource import Resource
 from twisted.web.server import Site
 from twisted.web.static import File
 
-TUTORIALS_DIRECTORY = filepath.FilePath(__file__).parent().sibling("tutorials/")
+SITE_DIRECTORY = filepath.FilePath(__file__).parent().sibling("tutorials/")
 
-tutorials = File(TUTORIALS_DIRECTORY.path, )
+tutorials = File(SITE_DIRECTORY.path, )
 
 class Error(Resource):
     def __init__(self, path, code):
         Resource.__init__(self)
+        print "Error:", path, code
         self.path = path
         self.code = code
 
@@ -36,42 +39,50 @@ class TutorialPage(Resource):
 
     def __init__(self, path):
         Resource.__init__(self)
+        print "TutorialPage:", path
         self.path = path
     
     def render_GET(self, request):
-        return """
-        <html>
-        <body>
-        %s
-        </body>
-        </html>
-        """ %(self.path,)
+        print "TP_GET:", self.path, request
+        f = open(self.path,"r")
+        return f.read()
 
 
 class TutorialDirectory(Resource):
     def __init__(self, path):
         Resource.__init__(self)
+        print "TutorialDirectory:", path
         self.path = path
 
     def getChild(self, path, request):
-        if 
-
+        path = os.path.join(SITE_DIRECTORY, self.path, path)
+        print "TD:", path, request
+        if os.path.isdir(path):
+            return TutorialDirectory(path)
+        elif os.path.exists(path):
+            return TutorialPage(path)
+        else:
+            return Resource.getChild(self, path, request)
+            
 
 class Root(Resource):
     def __init__(self):
         Resource.__init__(self)
+        print "Root init"
 
     def getChild(self, path, request):
-        if path.lower().endswith("shtml"):
+        path = os.path.join(SITE_DIRECTORY.path, path)
+        print "Root:", path, request
+        if os.path.isdir(path):
+            return TutorialDirectory(path)
+        elif os.path.exists(path):
             return TutorialPage(path)
         else:
-            return TutorialDirectory(path)
-        else:
-            return Error(path, http.NOT_FOUND)
+            return Resource.getChild(self, path, request)
 
 
 if __name__=="__main__":
-    print "Starting to monitor %s:"%(TUTORIALS_DIRECTORY,)
+    print "Starting to monitor %s:"%(SITE_DIRECTORY,)
     root = Root()
     factory = Site(root)
     reactor.listenTCP(8880, factory)
